17921bfca70a27edb76f5ae2d4cf8b0369856cc0
LFKilian <lefloch.k@stjolorient.com>
2026-05-20 15:42:19 +0200

    feat: ajout du système de logs locaux et suppression automatique du JSON après envoi

--- send_api.py
+++ send_api.py
@@ -9,7 +9,9 @@ date_actuelle = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 token = "J1Yz5Ic6Ht+koGlVOukLeQ$gF75BH8vAEX3NrEvy/OV5KzvcJSIYcAYUh/vSbQ5Gkk"
 rasp_id="rasp1"
 RESULTS_DIR = PROJECT_ROOT / "Final" / "results"
+LOGS_DIR = PROJECT_ROOT / "Final" / "logs"
 path_json = RESULTS_DIR / "resultats.jsonl"
+path_log = LOGS_DIR / "logs.jsonl"
 
 def envoyer_analyse_api():
 
@@ -51,8 +53,69 @@ def envoyer_analyse_api():
                         succes = True
                     else:
                         print(f"API : Erreur {response.status_code} : {response.text}")
+                        ajouter_log(f"API : Erreur {response.status_code} : {response.text}")
                         succes = False
+                
+        if succes == True:
+            os.remove(path_json)
+            print(f"API : Succès !")
+            ajouter_log("API : Succès !")
             
     except Exception as e:
         print(f"API : Échec de la connexion : {e}")
+        ajouter_log(f"API : Échec de la connexion : {e}")
 
+
+def ajouter_log(message):
+   
+    nouvelle_ligne = {
+        "timestamp": date_actuelle,
+        "message": str(message)
+    }
+    
+    with open(path_log, 'a', encoding='utf-8') as f:
+        f.write(json.dumps(nouvelle_ligne, ensure_ascii=False) + '\n') 
+
+"""
+def envoyer_log_api():
+
+    url = "http://10.0.200.78:8000/SendLog"
+
+    if not os.path.exists(path_log):
+        print(f"Erreur : {path_log} introuvable")
+        return
+    
+    try:
+        # Lecture et chargement du fichier JSON
+        with open(path_log, 'r', encoding='utf-8') as f:
+            for ligne in f:
+                donnees_json = json.loads(ligne)
+
+                actions = donnees_json.get('message')
+                if actions:
+                    actions_log.append(actions)
+
+                for log in actions_logs:
+                
+                    # Paramètres FastAPI
+                    parametres = {
+                        "date": str(date_actuelle),
+                        "message": str(log),
+                        "rasp": str(rasp_id),
+                        "token": str(token)
+                    }
+                    
+                    # Envoi
+                    response = requests.post(url, params=parametres, timeout=5)
+                    if response.status_code in [200, 201]:
+                        print(f"API : Succès !")
+                    else:
+                        print(f"API : Erreur {response.status_code} : {response.text}")
+                        
+        if succes == True:
+            os.remove(path_json)
+            print(f"API : Succès !")
+            ajouter_log("API : Succès !")                
+
+    except Exception as e:
+        print(f"API : Échec de la connexion : {e}") """
